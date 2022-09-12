3f283d15c7a366086be89fcf109e240c48a86586
sodamnfoolish <n.zhyikov@gmail.com>
2022-09-12 07:33:59 +0300

    Configured hardhat-contract-sizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,9 +1,13 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import '@openzeppelin/hardhat-upgrades';
+import "@openzeppelin/hardhat-upgrades";
+import "hardhat-contract-sizer";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.16",
+  contractSizer: {
+    runOnCompile: true
+  }
 };
 
 export default config;
