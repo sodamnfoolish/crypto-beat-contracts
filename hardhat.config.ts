f0f57ad4b0d5332620b3c749b0fb1c0bad4f3a81
sodamnfoolish <n.zhyikov@gmail.com>
2022-09-12 07:56:35 +0300

    Configured hardhat-gas-reporter

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,11 +3,15 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 import "hardhat-contract-sizer";
 import "solidity-coverage";
+import "hardhat-gas-reporter";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.16",
   contractSizer: {
     runOnCompile: true
+  },
+  gasReporter: {
+    enabled: true
   }
 };
 
