570c367975fba8d6a96ee299bf6afa4afc690db2
sodamnfoolish <n.zhyikov@gmail.com>
2022-09-12 07:25:02 +0300

    Created TestERC20.sol

---
+++ contracts/TestERC20.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.16;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestERC20 is ERC20 {
+    constructor() ERC20("TestERC20", "TERC20") {
+        _mint(msg.sender, type(uint256).max);
+    }
+}
