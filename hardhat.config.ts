da474f34c00f22bb01efa2e3d6d72c2923550492
sodamnfoolish <n.zhyikov@gmail.com>
2022-09-12 07:53:08 +0300

    Configured solidity-coverage

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,6 +2,7 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 import "hardhat-contract-sizer";
+import "solidity-coverage";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.16",
