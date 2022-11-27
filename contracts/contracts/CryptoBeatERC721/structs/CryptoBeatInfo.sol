92ea2b2021e140fd2fe7b96f885ff85cf40afaba
sodamnfoolish <n.zhyikov@gmail.com>
2022-11-27 15:18:51 +0200

    feat: created CryptoBeatERC721

---
+++ contracts/contracts/CryptoBeatERC721/structs/CryptoBeatInfo.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+struct CryptoBeatInfo {
+    string name;
+    string mp3Url;
+    string wavUrl;
+    string trackStemsUrl;
+    bytes32 license;
+}
