2bec2a942db19e46d5fb6fa03d0b2e2548292313
sodamnfoolish <n.zhyikov@gmail.com>
2022-12-02 18:55:02 +0200

    feat: created CryptoBeatMarketplace.addCryptoBeat

--- contracts/CryptoBeatMarketplace/CryptoBeatMarketplace.sol
+++ contracts/CryptoBeatMarketplace/CryptoBeatMarketplace.sol
@@ -6,14 +6,33 @@ import "../CryptoBeatGovernance/presets/CryptoBeatGovernanceInjected.sol";
 import "../CryptoBeatMembers/presets/CryptoBeatMembersInjected.sol";
 import "../CryptoBeatLicensing/presets/CryptoBeatLicensingInjected.sol";
 import "../CryptoBeatERC721/presets/CryptoBeatERC721Injected.sol";
+import "./structs/CryptoBeatTokenOnMarketplaceInfo.sol";
 
 contract CryptoBeatMarketplace is CryptoBeatGovernanceInjected, CryptoBeatMembersInjected, CryptoBeatLicensingInjected, CryptoBeatERC721Injected {
+    mapping(uint256 => CryptoBeatTokenOnMarketplaceInfo) _cryptoBeatTokenOnMarketplaceInfos;
+
     uint256[50] private __gap;
 
-    function initialize(CryptoBeatGovernance cryptoBeatGovernance, CryptoBeatMembers cryptoBeatMembers, CryptoBeatLicensing cryptoBeatLicensing, CryptoBeatERC721 cryptoBeatERC721) external initializer {
+    event AddCryptoBeat(address cryptoBeatMember, uint256 cryptoBeatTokenId);
+
+    function initialize(
+            CryptoBeatGovernance cryptoBeatGovernance, 
+            CryptoBeatMembers cryptoBeatMembers, 
+            CryptoBeatLicensing cryptoBeatLicensing, 
+            CryptoBeatERC721 cryptoBeatERC721) 
+        external initializer {
         __CryptoBeatGovernanceInjected_init(cryptoBeatGovernance);
         __CryptoBeatMembersInjected_init(cryptoBeatMembers);
         __CryptoBeatLicensingInjected_init(cryptoBeatLicensing);
         __CryptoBeatERC721Injected_init(cryptoBeatERC721);
+    }
+
+    function addCryptoBeat(uint256 cryptoBeatTokenId) external onlyCryptoBeatMemberNotBanned onlyCryptoBeatTokenOwner(cryptoBeatTokenId) onlyExclusiveLicenseCryptoBeatToken(cryptoBeatTokenId) onlyApprovedCryptoBeatToken(cryptoBeatTokenId) {
+        _cryptoBeatERC721.transferFrom(msg.sender, address(this), cryptoBeatTokenId);
+
+        _cryptoBeatTokenOnMarketplaceInfos[cryptoBeatTokenId].isOn = true;
+        _cryptoBeatTokenOnMarketplaceInfos[cryptoBeatTokenId].owner = msg.sender;
+
+        emit AddCryptoBeat(msg.sender, cryptoBeatTokenId);
     }
 }
