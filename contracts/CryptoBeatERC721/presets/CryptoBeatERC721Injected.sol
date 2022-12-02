856601d08a9d6a6ab3a8d5b8f41700724e7f16af
sodamnfoolish <n.zhyikov@gmail.com>
2022-12-02 18:45:03 +0200

    Revert "feat: created onlyApprovedCryptoBeatToken"
    This reverts commit dc74074de3a5cbcf19b7c2a840b8921184ef69e7.

--- contracts/CryptoBeatERC721/presets/CryptoBeatERC721Injected.sol
+++ contracts/CryptoBeatERC721/presets/CryptoBeatERC721Injected.sol
@@ -10,43 +10,21 @@ contract CryptoBeatERC721Injected is Initializable {
 
     uint256[50] private __gap;
 
-    function __CryptoBeatERC721Injected_init(
-        CryptoBeatERC721 cryptoBeatERC721
-    ) internal onlyInitializing {
+    function __CryptoBeatERC721Injected_init(CryptoBeatERC721 cryptoBeatERC721) internal onlyInitializing {
         __CryptoBeatERC721Injected_init_unchained(cryptoBeatERC721);
     }
 
-    function __CryptoBeatERC721Injected_init_unchained(
-        CryptoBeatERC721 cryptoBeatERC721
-    ) internal onlyInitializing {
+    function __CryptoBeatERC721Injected_init_unchained(CryptoBeatERC721 cryptoBeatERC721) internal onlyInitializing {
         _cryptoBeatERC721 = cryptoBeatERC721;
     }
 
     modifier onlyCryptoBeatTokenOwner(uint256 cryptoBeatTokenId) {
-        require(
-            _cryptoBeatERC721.ownerOf(cryptoBeatTokenId) == msg.sender,
-            "CryptoBeatERC721Injected: only CryptoBeatToken owner"
-        );
+        require(_cryptoBeatERC721.ownerOf(cryptoBeatTokenId) == msg.sender, "CryptoBeatERC721Injected: only CryptoBeatToken owner");
         _;
     }
 
     modifier onlyExclusiveLicenseCryptoBeatToken(uint256 cryptoBeatTokenId) {
-        require(
-            _cryptoBeatERC721
-                .getCryptoBeatTokenInfo(cryptoBeatTokenId)
-                .licenseId == CryptoBeatLicenses.EXCLUSIVE_LICENSE_ID,
-            "CryptoBeatERC721Injected: only CryptoBeatToken with Exclusive license"
-        );
-        _;
-    }
-
-    modifier onlyApprovedCryptoBeatToken(uint256 cryptoBeatTokenId) {
-        require(
-            _cryptoBeatERC721.isApprovedForAll(msg.sender, address(this)) ||
-                _cryptoBeatERC721.getApproved(cryptoBeatTokenId) ==
-                address(this),
-            "CryptoBeatERC721Injected: only approved CryptoBeatToken"
-        );
+        require(_cryptoBeatERC721.getCryptoBeatTokenInfo(cryptoBeatTokenId).licenseId == CryptoBeatLicenses.EXCLUSIVE_LICENSE_ID, "CryptoBeatERC721Injected: only CryptoBeatToken with Exclusive license");
         _;
     }
 }
